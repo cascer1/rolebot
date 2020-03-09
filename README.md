55950f29e62a420c8180b667340ff27ed34585f6
Cas Eliëns <cas.eliens@isaac.nl>
2020-03-09 09:22:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rolebot
+A bot to mass-assign roles in a Discord server.
+
+## Usage
+
+`rolebot assign <required> <newRole>`
+
+for example: `rolebot assign @everyone @awesome`, or `rolebot assign @member @approved`
+
+## Permissions
+Only members with the role management permission are allowed to use this bot. 
---
+++ src/main/java/com/dongtronic/rolebot/AssignCommand.kt
@@ -0,0 +1,75 @@
+package com.dongtronic.rolebot
+
+import com.jagrosh.jdautilities.command.Command
+import com.jagrosh.jdautilities.command.CommandEvent
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.Role
+
+class AssignCommand() : Command() {
+
+    init {
+        this.name = "assign"
+        this.help = "Mass-assign roles"
+        this.guildOnly = true
+        this.arguments = "<required role> <new role>"
+        this.userPermissions = arrayOf(Permission.MANAGE_ROLES)
+    }
+
+    override fun execute(event: CommandEvent) {
+        if (event.args.isEmpty()) {
+            event.replyWarning("You didn't give me any arguments!")
+        } else {
+            // split the arguments on all whitespaces
+            val items = event.args.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+            if (items.size < 2) {
+                event.replyWarning("Required arguments: `current role` and `new role`. For example: `rolebot assign @everyone @awesome`")
+                return
+            }
+
+            doTheThing(event)
+        }
+    }
+
+    private fun doTheThing(event: CommandEvent) {
+        val roles = event.message.mentionedRoles
+        val everyone = event.message.mentionsEveryone()
+        var count = 0
+        var givenRole: Role
+
+        if (everyone) {
+            if (roles.size != 1) {
+                event.replyError("Only one role may be assigned at a time")
+                return
+            }
+
+            givenRole = roles[0]
+            count = assignRoles(null, givenRole, event.guild)
+        } else {
+
+            if (roles.size != 2) {
+                event.replyError("Must provide two roles")
+                return
+            }
+
+            givenRole = roles[1]
+            count = assignRoles(roles[0], givenRole, event.guild)
+        }
+
+        event.replySuccess("Assigned role `${givenRole.name}` to $count members")
+
+    }
+
+    private fun assignRoles(required: Role?, assigned: Role, guild: Guild): Int {
+        val members = if (required == null) {
+            guild.members
+        } else {
+            guild.getMembersWithRoles(required)
+        }
+
+        members.stream().forEach { member -> guild.addRoleToMember(member, assigned).reason("Assigned by Rolebot").queue() }
+
+        return members.size
+    }
+}
